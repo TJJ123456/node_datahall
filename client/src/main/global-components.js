364e2ac82ca90f97ee3871f56a7856630e56b9fe
tjj <443111954@qq.com>
2019-05-28 11:03:28 +0800

    学习借鉴别人的主页

--- client/src/main/global-components.js
+++ client/src/main/global-components.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import headTop from '../components/headTop.vue';
+import SvgIcon from '../components/SvgIcon'// svg component
 
 Vue.use(ElementUI);
 Vue.component('headTop', headTop);
+Vue.component('svg-icon', SvgIcon)
--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,18 +1,49 @@
 <template>
   <div>
     <headTop/>
-    <h1>数据堂后台管理中心</h1>
+    <div class="dashboard-editor-container">
+      <panel-group @handleSetLineChartData="handleSetLineChartData"/>
+      <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+        <line-chart :chart-data="lineChartData"/>
+      </el-row>
+      <h1>数据堂后台管理中心</h1>
+    </div>
   </div>
 </template>
 <script>
+import PanelGroup from "../components/PanelGroup";
+import LineChart from "../components/LineChart";
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+};
 export default {
   data() {
     return {
       bookCount: 0,
       bookshelfCount: 0,
       genreCount: 0,
-      userCount: 0
+      userCount: 0,
+      lineChartData: lineChartData.newVisitis
     };
+  },
+  components: {
+    PanelGroup,
+    LineChart
   },
   created() {
     // this.initData();
@@ -31,7 +62,17 @@ export default {
       this.bookshelfCount = bookshelfCount.data;
       this.genreCount = genreCount.data;
       this.userCount = userCount.data;
+    },
+    handleSetLineChartData(type) {
+      this.lineChartData = lineChartData[type];
     }
   }
 };
 </script>
+<style lang="less" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+}
+</style>
